d49c467698e0483f2fce16696ef430b9ab4d3c21
kms <kmslkh@naver.com>
2021-08-25 18:37:58 +0900

    feat (Valid 구현)

--- src/main/java/utils/Valid.java
+++ src/main/java/utils/Valid.java
@@ -1,11 +1,27 @@
 package utils;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 public class Valid {
+
     public Boolean numberValid(String input) {
-        return true;
+        String pattern = "^[1-9]{3}";
+        Set<Character> set = new HashSet<Character>() ;
+        if (input == null || input.isEmpty() || !Pattern.matches(pattern, input)) {
+            return false;
+        }
+        for (char ch : input.toCharArray())
+            set.add(ch);
+        return set.size() == 3;
     }
 
     public Boolean exitStatusValid(String input) {
-        return true;
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+        input = input.toUpperCase();
+        return "Y".equals(input) || "N".equals(input);
     }
 }
