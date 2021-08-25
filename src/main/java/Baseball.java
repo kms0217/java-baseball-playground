d27ba89045a71a634eb4ecb774c9fdb4a86c97cd
kms <kmslkh@naver.com>
2021-08-25 20:24:00 +0900

    feat (Game, Main)

---
+++ src/main/java/Baseball.java
@@ -0,0 +1,9 @@
+import game.Game;
+
+public class Baseball {
+
+    public static void main(String[] args) {
+        Game game = Game.getInstance();
+        game.start();
+    }
+}
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,20 +1,82 @@
 package game;
 
+import utils.NumGenerator;
+import view.InputView;
+import view.ResultView;
+
 public class Game {
 
-    public void start() {
+    private static Game game = null;
+    private static InputView inputView;
+    private static ResultView resultView;
+    private static NumGenerator generator;
+    private String generatedNum;
 
+    private Game() {
+    }
+
+    private static void init() {
+        game = new Game();
+        inputView = new InputView();
+        resultView = new ResultView();
+        generator = new NumGenerator();
+    }
+
+    public static Game getInstance() {
+        if (game == null) {
+            init();
+        }
+        return game;
+    }
+
+    public void start() {
+        int strikeNum;
+        int ballNum;
+        Boolean end;
+        String input;
+        generatedNum = generator.generate();
+        do {
+            input = inputView.inputNumber();
+            strikeNum = countStrike(generatedNum, input);
+            ballNum = countBall(generatedNum, input);
+            resultView.resultPrint(strikeNum, ballNum);
+            end = checkEnd(strikeNum);
+        } while (!end);
     }
 
     public int countBall(String com, String user) {
-        return 0;
+        int ret = 0;
+        if (com.charAt(0) == user.charAt(1) || com.charAt(0) == user.charAt(2)) {
+            ret++;
+        }
+        if (com.charAt(1) == user.charAt(0) || com.charAt(1) == user.charAt(2)) {
+            ret++;
+        }
+        if (com.charAt(2) == user.charAt(0) || com.charAt(2) == user.charAt(1)) {
+            ret++;
+        }
+        return ret;
     }
 
     public int countStrike(String com, String user) {
-        return 0;
+        int ret = 0;
+        if (com.charAt(0) == user.charAt(0)) {
+            ret++;
+        }
+        if (com.charAt(1) == user.charAt(1)) {
+            ret++;
+        }
+        if (com.charAt(2) == user.charAt(2)) {
+            ret++;
+        }
+        return ret;
     }
 
     public Boolean checkEnd(int strikeNum) {
+        if (strikeNum == 3) {
+            generatedNum = generator.generate();
+            return inputView.inputExitStatus();
+        }
         return false;
     }
 }
